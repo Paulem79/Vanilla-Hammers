ec137f3c15a7d4c602f91aa1d312798618e37d5c
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-06-07 17:35:37 +0200

    Initial commit

---
+++ src/main/kotlin/net/paulem/vanillahammers/VanillaHammers.kt
@@ -0,0 +1,14 @@
+package net.paulem.vanillahammers
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class VanillaHammers : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
